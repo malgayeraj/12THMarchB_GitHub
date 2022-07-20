edfc2887b40bb8ea37ef69deeeeb1a7113a1a70b
hp <hp@DESKTOP-0NHAKSM>
2022-07-19 21:50:04 -0500

    [Rajeshree]: added new project

---
+++ src/Sample1/Demo.java
@@ -0,0 +1,17 @@
+package Sample1;
+
+public class Demo
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hiii");
+
+		System.out.println("Hello");
+		
+		System.out.println("Hii");
+		
+		System.out.println("Hello");
+	}
+	
+
+}
